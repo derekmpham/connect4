05814fb034e125a2c43e6b3d0bb2087f5626c0be
Derek <derekmpham@gmail.com>
2021-09-25 21:45:04 -0700

    Add test for invalid move: not player's turn (complex test)

--- Skeleton/test_Gameboard.py
+++ Skeleton/test_Gameboard.py
@@ -2,7 +2,7 @@ import unittest
 from Gameboard import Gameboard
 
 class Test_TestGameboard(unittest.TestCase):
-    def test_simple_correct_move(self):
+    def test_correct_move_simple(self):
         # Checks single correct move for each player
         game = Gameboard()
         game.player1 = "red"
@@ -20,7 +20,7 @@ class Test_TestGameboard(unittest.TestCase):
         p2_valid_status = game.validate_move(p2_m, game.current_turn)
         self.assertFalse(p2_valid_status[0])
 
-    def test_complex_correct_moves(self):
+    def test_correct_moves_complex(self):
         # Checks for all correct moves in a tie board
         moves = ['col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7',
                 'col2', 'col1', 'col1', 'col2', 'col2', 'col1', 'col1',
@@ -47,7 +47,7 @@ class Test_TestGameboard(unittest.TestCase):
             self.assertEqual(game.game_result, "")
             game.switch_turn()
 
-    def test_invalid_move_turn(self):
+    def test_invalid_move_turn_simple(self):
         # Checks that player cannot make a move when it is not his/her turn
         game = Gameboard()
         game.player1 = "red"
@@ -62,6 +62,37 @@ class Test_TestGameboard(unittest.TestCase):
 
         p1_valid_status = game.validate_move('col1', 'p1')
         self.assertEqual(p1_valid_status[1], "Not your turn")
+
+    def test_invalid_move_turn_complex(self):
+        # Checks that for every turn (42 total turns) player cannot make
+        # move when it is not his/her turn
+        game = Gameboard()
+        game.player1 = "red"
+        game.player2 = "yellow"
+        moves = ['col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7',
+                'col2', 'col1', 'col1', 'col2', 'col2', 'col1', 'col1',
+                'col2', 'col1', 'col2', 'col6', 'col3', 'col3', 'col3',
+                'col3', 'col4', 'col4', 'col4', 'col4', 'col5', 'col5',
+                'col5', 'col5', 'col4', 'col3', 'col7', 'col7', 'col7',
+                'col7', 'col6', 'col6', 'col6', 'col7', 'col6', 'col5']
+        
+        while game.remaining_moves > 0:
+            m = moves.pop(0)
+            
+            pcolor = ""
+            wrong_person = ''
+            if game.current_turn == 'p1':
+                pcolor = game.player1
+                wrong_person = 'p2'
+            else:
+                pcolor = game.player2
+                wrong_person = 'p1'
+
+            p_valid_status = game.validate_move(m, wrong_person)
+            self.assertEqual(p_valid_status[1], "Not your turn")
+
+            added_pos = game.add_move(m, pcolor)
+            game.switch_turn()
 
 
 if __name__ == '__main__':
